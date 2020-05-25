00ada1079286976f43c2cfbffcdd32857fb1c0e9
vankatwijk <hendrikus.hpvk@gmail.com>
2020-05-25 16:30:06 +0200

    setting up laravel and vue.js

---
+++ laravelbnb/resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>LaravelBnb</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </head>
+    <body>
+        <div id="app"></div>
+    </body>
+</html>
