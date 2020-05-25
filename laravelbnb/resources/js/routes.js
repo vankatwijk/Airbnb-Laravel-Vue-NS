7b72ad0f18f54e6dcd55a6a228c28ccb5c03b54b
vankatwijk <hendrikus.hpvk@gmail.com>
2020-05-25 17:16:32 +0200

    setting up vue router

---
+++ laravelbnb/resources/js/routes.js
@@ -0,0 +1,17 @@
+import ExampleComponent from "./components/ExampleComponent";
+import VueRouter from "vue-router";
+
+const routes = [
+    {
+        path: "/",
+        component:ExampleComponent,
+        name:"home",
+    }
+];
+
+const router = new VueRouter({
+    routes,
+});
+
+
+export default router;
--- laravelbnb/resources/views/welcome.blade.php
+++ laravelbnb/resources/views/welcome.blade.php
@@ -12,6 +12,8 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
-        <div id="app"></div>
+        <div id="app">
+            <router-view></router-view>
+        </div>
     </body>
 </html>
