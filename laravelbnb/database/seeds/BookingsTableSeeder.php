f2a2205b7de8d24ee72fbf05ca70b1a64b3b930a
vankatwijk <hendrikus.hpvk@gmail.com>
2020-06-10 18:36:08 +0200

    added seeders

---
+++ laravelbnb/database/seeds/BookingsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BookingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Bookable::all()->each(function (Bookable $bookable)){
+            $booking = factory(Booking::class)->make();
+            $bookings = collect([$booking]);
+
+            for($i=0; $i < random_int(1,20); $i++){
+                $from = (clone $booking-to)->addDays(random_int(1,14));
+                $to = (clone $from)->addDays(random_int(0,14));
+
+                $booking = new Booking();
+                $booking->from = $from;
+                $booking = Booking::make([
+
+                ]);
+            }
+        }
+    }
+}
--- laravelbnb/database/seeds/DatabaseSeeder.php
+++ laravelbnb/database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UserSeeder::class);
         $this->call(BookablesTableSeeder::class);
+        $this->call(BookingTableSeeder::class);
     }
 }
