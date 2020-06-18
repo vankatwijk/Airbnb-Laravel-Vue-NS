921a9efdac73946c5ec42731b88e40f3b9bfea98
vankatwijk <hendrikus.hpvk@gmail.com>
2020-06-18 15:52:10 +0200

    added factory and seeders to laravel

--- laravelbnb/database/seeds/DatabaseSeeder.php
+++ laravelbnb/database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UserSeeder::class);
         $this->call(BookablesTableSeeder::class);
         $this->call(BookingsTableSeeder::class);
+        $this->call(ReviewsTableSeeder::class);
     }
 }
---
+++ laravelbnb/database/seeds/ReviewsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Review;
+use Illuminate\Database\Seeder;
+
+class ReviewsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Bookable::all()->each(function (Bookable $bookable){
+            $reviews = factory(Review::class, random_int(5, 30))->make();
+
+            $bookable->reviews()->saveMany($reviews);
+
+        });
+    }
+}
