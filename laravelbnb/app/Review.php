0eae217ab9860472fc4ec08d612e16c70130ec3d
vankatwijk <hendrikus.hpvk@gmail.com>
2020-06-18 15:01:50 +0200

    added model migration with uuid

---
+++ laravelbnb/app/Review.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    public function getIncrementing(){
+        return false;
+    }
+
+    public function getKeyType(){
+        return 'string';
+    }
+}
