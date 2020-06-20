7716dd75a0d046f5b33a7dfe2cab0868852f2487
vankatwijk <hendrikus.hpvk@gmail.com>
2020-06-20 18:22:14 +0200

    Get review exist

--- laravelbnb/app/Http/Controllers/Api/ReviewController.php
+++ laravelbnb/app/Http/Controllers/Api/ReviewController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers\Api;
 use App\Review;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ReviewResource;
 
 class ReviewController extends Controller
 {
     public function show($id){
 
-        return Review::findOrFail($id);
+        return new ReviewResource(Review::findOrFail($id));
     }
 }
--- laravelbnb/routes/api.php
+++ laravelbnb/routes/api.php
@@ -21,3 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::apiResource('bookables', 'Api\BookableController')->only(['index','show']); 
 Route::get('bookables/{bookable}/availability','Api\BookableAvailabilityController')->name('bookables.availability.show');
 Route::get('bookables/{bookable}/reviews', 'Api\BookableReviewController')->name('bookables.reviews.index');
+
+Route::apiResource('reviews','Api\ReviewController')->only(['show']);
