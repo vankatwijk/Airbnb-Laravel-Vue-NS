8e0050519e4df413f68e6aa9e48c21a11c000d04
vankatwijk <hendrikus.hpvk@gmail.com>
2020-06-21 22:36:03 +0200

    check if already reviewed

--- laravelbnb/public/js/app.js
+++ laravelbnb/public/js/app.js
@@ -2261,16 +2261,44 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
       review: {
         rating: 5,
         content: null
-      }
+      },
+      existingReview: null,
+      loading: false
     };
   },
-  created: function created() {}
+  created: function created() {
+    var _this = this;
+
+    this.loading = true;
+    axios.get("/api/reviews/".concat(this.$route.params.id)).then(function (response) {
+      return _this.existingReview = response.data.data;
+    })["catch"](function (err) {}).then(function () {
+      return _this.loading = false;
+    });
+  },
+  computed: {
+    alreadyReviewed: function alreadyReviewed() {
+      return this.existingReview !== null;
+    }
+  }
 });
 
 /***/ }),
@@ -60154,59 +60182,75 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("div", [
-    _c(
-      "div",
-      { staticClass: "form-group" },
-      [
-        _c("label", { staticClass: "text-muted" }, [
-          _vm._v("select the star rating")
-        ]),
-        _vm._v(" "),
-        _c("star-rating", {
-          staticClass: "fa-4x",
-          model: {
-            value: _vm.review.rating,
-            callback: function($$v) {
-              _vm.$set(_vm.review, "rating", $$v)
-            },
-            expression: "review.rating"
-          }
-        })
-      ],
-      1
-    ),
-    _vm._v(" "),
-    _c("div", { staticClass: "form-group" }, [
-      _c("label", { staticClass: "text-muted", attrs: { for: "content" } }, [
-        _vm._v("\n            Describe  your expirience with\n        ")
-      ]),
-      _vm._v(" "),
-      _c("textarea", {
-        directives: [
-          {
-            name: "model",
-            rawName: "v-model",
-            value: _vm.review.content,
-            expression: "review.content"
-          }
-        ],
-        staticClass: "form-control",
-        attrs: { name: "content", cols: "30", rows: "10" },
-        domProps: { value: _vm.review.content },
-        on: {
-          input: function($event) {
-            if ($event.target.composing) {
-              return
-            }
-            _vm.$set(_vm.review, "content", $event.target.value)
-          }
-        }
-      })
-    ]),
-    _vm._v(" "),
-    _c("button", { staticClass: "btn btn-lg btm-primary btn-block" }, [
-      _vm._v("Submit")
-    ])
+    _vm.loading
+      ? _c("div", [_vm._v("\n        Loading...\n    ")])
+      : _c("div", [
+          _vm.alreadyReviewed
+            ? _c("div", [_c("h3", [_vm._v("You've already reviews this")])])
+            : _c("div", [
+                _c(
+                  "div",
+                  { staticClass: "form-group" },
+                  [
+                    _c("label", { staticClass: "text-muted" }, [
+                      _vm._v("select the star rating")
+                    ]),
+                    _vm._v(" "),
+                    _c("star-rating", {
+                      staticClass: "fa-4x",
+                      model: {
+                        value: _vm.review.rating,
+                        callback: function($$v) {
+                          _vm.$set(_vm.review, "rating", $$v)
+                        },
+                        expression: "review.rating"
+                      }
+                    })
+                  ],
+                  1
+                ),
+                _vm._v(" "),
+                _c("div", { staticClass: "form-group" }, [
+                  _c(
+                    "label",
+                    { staticClass: "text-muted", attrs: { for: "content" } },
+                    [
+                      _vm._v(
+                        "\n                    Describe  your expirience with\n                "
+                      )
+                    ]
+                  ),
+                  _vm._v(" "),
+                  _c("textarea", {
+                    directives: [
+                      {
+                        name: "model",
+                        rawName: "v-model",
+                        value: _vm.review.content,
+                        expression: "review.content"
+                      }
+                    ],
+                    staticClass: "form-control",
+                    attrs: { name: "content", cols: "30", rows: "10" },
+                    domProps: { value: _vm.review.content },
+                    on: {
+                      input: function($event) {
+                        if ($event.target.composing) {
+                          return
+                        }
+                        _vm.$set(_vm.review, "content", $event.target.value)
+                      }
+                    }
+                  })
+                ]),
+                _vm._v(" "),
+                _c(
+                  "button",
+                  { staticClass: "btn btn-lg btm-primary btn-block" },
+                  [_vm._v("Submit")]
+                )
+              ])
+        ])
   ])
 }
 var staticRenderFns = []
--- laravelbnb/resources/js/review/Review.vue
+++ laravelbnb/resources/js/review/Review.vue
@@ -1,19 +1,31 @@
 <template>
     <div>
-        <div class="form-group">
-            <label class="text-muted">select the star rating</label>
-            <star-rating
-            class="fa-4x" 
-            v-model="review.rating"
-            ></star-rating>
+        <div v-if="loading">
+            Loading...
         </div>
-        <div class="form-group">
-            <label for="content" class="text-muted">
-                Describe  your expirience with
-            </label>
-            <textarea name="content" cols="30" rows="10" class="form-control" v-model="review.content"></textarea>
+        <div v-else>
+            
+            <div v-if="alreadyReviewed">
+                <h3>You've already reviews this</h3>
+            </div>
+            <div v-else>
+                <div class="form-group">
+                    <label class="text-muted">select the star rating</label>
+                    <star-rating
+                    class="fa-4x" 
+                    v-model="review.rating"
+                    ></star-rating>
+                </div>
+                <div class="form-group">
+                    <label for="content" class="text-muted">
+                        Describe  your expirience with
+                    </label>
+                    <textarea name="content" cols="30" rows="10" class="form-control" v-model="review.content"></textarea>
+                </div>
+                <button class="btn btn-lg btm-primary btn-block">Submit</button>
+            </div>
+
         </div>
-        <button class="btn btn-lg btm-primary btn-block">Submit</button>
     </div>
 </template>
 
@@ -25,11 +37,23 @@ export default {
             review:{
                 rating:5,
                 content:null
-            }
+            },
+            existingReview:null,
+            loading:false
         }
     },
     created(){
-        
+        this.loading = true;
+        axios.get(`/api/reviews/${this.$route.params.id}`)
+        .then(response => (this.existingReview = response.data.data))
+        .catch(err => {
+
+        }).then(() => (this.loading = false));
+    },
+    computed: {
+        alreadyReviewed(){
+            return this.existingReview !== null;
+        }
     }
 }
 </script>
