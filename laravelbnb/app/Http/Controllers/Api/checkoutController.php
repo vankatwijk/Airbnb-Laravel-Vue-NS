60f61f8e4c3caf33ca921f03a35bf7175bd0f030
vankatwijk <hendrikus.hpvk@gmail.com>
2020-07-30 12:48:58 +0200

    check booking dates still available in backend

--- laravelbnb/app/Http/Controllers/Api/checkoutController.php
+++ laravelbnb/app/Http/Controllers/Api/checkoutController.php
@@ -16,6 +16,10 @@ class checkoutController extends Controller
     public function __invoke(Request $request)
     {
         $data = $request->validate([
+            'bookings' => 'required|array|min:1',
+            'bookings.*.bookable_id' => 'required|exists:bookables,id',
+            'bookings.*.from' => 'required|date|after_or_equal:today',
+            'bookings.*.to' => 'required|date|after_or_equal:bookings.*.from',
             'customer.first_names' => 'required|min:2',
             'customer.last_name' => 'required|min:2',
             'customer.street' => 'required|min:3',
@@ -25,5 +29,17 @@ class checkoutController extends Controller
             'customer.state' => 'required|min:2',
             'customer.zip' => 'required|min:2'
         ]);
+
+        //check if the dates are stil valid
+        $data = array_merge($data, $request->validate([
+            'bookings.*' => ['required', function ($attribute, $value, $fail) {
+                $bookable = Bookable::findOrFail($value['bookable_id']);
+
+                if (!$bookable->availableFor($value['from'], $value['to'])) {
+                    $fail("The object is not available in given dates!");
+                }
+            }]
+        ]));
+
     }
 }
