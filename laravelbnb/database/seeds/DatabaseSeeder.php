fc63272737d15247b9b18e541cf5e9940182a120
vankatwijk <hendrikus.hpvk@gmail.com>
2020-08-11 10:24:31 +0200

    seeder for users table

--- laravelbnb/database/seeds/DatabaseSeeder.php
+++ laravelbnb/database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        $this->call(UserTableSeeder::class);
         $this->call(BookablesTableSeeder::class);
         $this->call(BookingsTableSeeder::class);
         $this->call(ReviewsTableSeeder::class);
