23e3ce52cb6296d4e7ab40ec4552a11e3fdbd3fc
vankatwijk <hendrikus.hpvk@gmail.com>
2020-08-11 09:58:28 +0200

    cors and js axios update

--- laravelbnb/public/js/app.js
+++ laravelbnb/public/js/app.js
@@ -79887,6 +79887,7 @@ try {
 
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true;
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
--- laravelbnb/resources/js/bootstrap.js
+++ laravelbnb/resources/js/bootstrap.js
@@ -22,6 +22,7 @@ try {
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true;
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
