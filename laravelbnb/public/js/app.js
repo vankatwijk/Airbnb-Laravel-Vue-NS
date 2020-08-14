2171ce026b90093508485c1750dca09622b6b4f0
vankatwijk <hendrikus.hpvk@gmail.com>
2020-08-14 17:06:55 +0200

    Fixing "is logged in" delay

--- laravelbnb/public/js/app.js
+++ laravelbnb/public/js/app.js
@@ -81108,6 +81108,8 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
       if (basket) {
         context.commit('setBasket', JSON.parse(basket));
       }
+
+      context.commit('setLoggedIn', Object(_shared_utils_auth__WEBPACK_IMPORTED_MODULE_1__["isLoggedIn"])());
     },
     addToBasket: function addToBasket(_ref, payload) {
       var commit = _ref.commit,
